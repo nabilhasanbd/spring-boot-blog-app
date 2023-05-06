9152bd4c936b1924b473fdb1583a5a0ddb0f0882
Nabil <nabil.uiu.bd@gmail.com>
2023-05-07 01:07:47 +0600

    Added properties

--- src/main/java/com/blogapp/SpringBootBlogAppApplication.java
+++ src/main/java/com/blogapp/SpringBootBlogAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootBlogAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootBlogAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBlogAppApplication.class, args);
+    }
 
 }
