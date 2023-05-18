e907a7b4da5c3fabee6de40f32478619c54de7a8
Nabil <nabil.uiu.bd@gmail.com>
2023-05-19 01:54:02 +0600

    Added Dto converter

--- src/main/java/com/blogapp/repositories/UserRepository.java
+++ src/main/java/com/blogapp/repositories/UserRepository.java
@@ -3,6 +3,6 @@ package com.blogapp.repositories;
 import com.blogapp.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserInterface extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
 }
--- src/main/java/com/blogapp/services/impl/UserServiceImplement.java
+++ src/main/java/com/blogapp/services/impl/UserServiceImplement.java
@@ -1,11 +1,16 @@
 package com.blogapp.services.impl;
 
+import com.blogapp.entities.User;
 import com.blogapp.payloads.UserDto;
+import com.blogapp.repositories.UserRepository;
 import com.blogapp.services.UserService;
 
 import java.util.List;
 
 public class UserServiceImplement implements UserService {
+
+    private UserRepository userRepo;
+
     @Override
     public UserDto createUser(UserDto user) {
         return null;
@@ -30,4 +35,24 @@ public class UserServiceImplement implements UserService {
     public void deleteUser(UserDto userId) {
 
     }
+    public User dtoToUser(UserDto userDto){
+        User user = new User();
+        user.setId(userDto.getId());
+        user.setName(userDto.getName());
+        user.setEmail(userDto.getEmail());
+        user.setPassword(userDto.getPassword());
+        user.setAbout(userDto.getAbout());
+        return user;
+    }
+
+    public UserDto userToDto(User user){
+        UserDto userDto = new UserDto();
+        userDto.setId(user.getId());
+        userDto.setName(user.getName());
+        userDto.setEmail(user.getEmail());
+        userDto.setPassword(user.getPassword());
+        userDto.setAbout(user.getAbout());
+        return userDto;
+    }
+
 }
